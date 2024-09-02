c8ca25ba2d915e9fdc6aeb27b4002e3daee6e356
MyPyCo <vibhavari.kulkarni@gmail.com>
2024-09-03 01:59:31 +0530

    Data Types.py

--- Data Types.py
+++ Data Types.py
@@ -2,7 +2,7 @@ print("Welcome to the tip calculator!")
 Totalbill = float(input("What was the total bill?\n$"))
 tip = int(input("How much tip would you like to give?10,12,15?\n"))
 num_people = int(input("How many to split the bill?\n"))
-Bill_perperson = (((Totalbill*0.10)+Totalbill)/num_people) 
+Bill_perperson = (((Totalbill*(tip*0.01)+Totalbill)/num_people) 
 print("Each person should pay: $",Bill_perperson)
 
 
